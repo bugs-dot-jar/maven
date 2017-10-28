7af39582ff607b9c5b426f50710a5b27df2c32b3
Bugs.jar <bugs.jar@us.fujitsu.com>
2017-10-27 19:31:24 -0700

    Added the benchmark files and applied the reverse patch to get the buggy version.

--- maven-model-builder/src/main/java/org/apache/maven/model/validation/DefaultModelValidator.java
+++ maven-model-builder/src/main/java/org/apache/maven/model/validation/DefaultModelValidator.java
@@ -764,47 +764,39 @@ public class DefaultModelValidator
             return true;
         }
 
-        if ( hasExpression( string ) )
+        if ( !hasExpression( string ) )
         {
-            addViolation( problems, severity, fieldName, sourceHint,
-                          "must be a valid version but is '" + string + "'.", tracker );
-            return false;
+            return true;
         }
 
-        if ( !validateBannedCharacters( fieldName, problems, severity, string, sourceHint, tracker,
-                                        ILLEGAL_VERSION_CHARS ) )
-        {
-            return false;
-        }
+        addViolation( problems, severity, fieldName, sourceHint, "must be a valid version but is '" + string + "'.",
+                      tracker );
 
-        return true;
+        return false;
     }
 
     private boolean validatePluginVersion( String fieldName, ModelProblemCollector problems, String string,
                                            String sourceHint, InputLocationTracker tracker,
                                            ModelBuildingRequest request )
     {
+        Severity errOn30 = getSeverity( request, ModelBuildingRequest.VALIDATION_LEVEL_MAVEN_3_0 );
+
         if ( string == null )
         {
             // NOTE: The check for missing plugin versions is handled directly by the model builder
             return true;
         }
 
-        Severity errOn30 = getSeverity( request, ModelBuildingRequest.VALIDATION_LEVEL_MAVEN_3_0 );
-
-        if ( !validateVersion( fieldName, problems, errOn30, string, sourceHint, tracker ) )
+        if ( string.length() > 0 && !hasExpression( string ) && !"RELEASE".equals( string )
+            && !"LATEST".equals( string ) )
         {
-            return false;
+            return true;
         }
 
-        if ( string.length() <= 0 || "RELEASE".equals( string ) || "LATEST".equals( string ) )
-        {
-            addViolation( problems, errOn30, fieldName, sourceHint, "must be a valid version but is '" + string + "'.",
-                          tracker );
-            return false;
-        }
+        addViolation( problems, errOn30, fieldName, sourceHint, "must be a valid version but is '" + string + "'.",
+                      tracker );
 
-        return true;
+        return false;
     }
 
     private static void addViolation( ModelProblemCollector problems, Severity severity, String fieldName,
